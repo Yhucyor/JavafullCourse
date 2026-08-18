862d0ca924641cade904dfc97ca5896c7ac9a23d
Nguyễn Trọng Thức <24110349@student.hcmute.edu.vn>
2026-08-18 21:08:46 +0700

    #OOP: Giới thiệu Class và Object trong lập trình hướng đối tượng OOP

---
+++ src/Java/OOP/Manager.java
@@ -0,0 +1,10 @@
+package Java.OOP;
+
+public class Manager {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Ngoc Minh";
+        s1.age = 10;
+        s1.printInfo();
+    }
+}
---
+++ src/Java/OOP/Student.java
@@ -0,0 +1,15 @@
+package Java.OOP;
+
+public class Student {
+//    === LẬP TRÌNH HƯỚNG ĐỐI TƯỢNG OOP === //
+    // Class là đối tượng tổng quát của real-world được mô phỏng
+    // Object là đối tượng cụ thể - thực thể chi tiết của thực thể đấy
+
+    String name;
+    int age;
+
+    public void printInfo(){
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
