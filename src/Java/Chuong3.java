a28550e87e0e6062a63395e5f01cd1b008f8f23e
Nguyễn Trọng Thức <24110349@student.hcmute.edu.vn>
2026-08-18 19:48:49 +0700

    #Chuong3: Vòng lặp For

---
+++ src/Java/Chuong3.java
@@ -0,0 +1,12 @@
+package Java;
+
+public class Chuong3 {
+    public static void main(String[] args) {
+        // CHƯƠNG 3: VÒNG LẶP //
+
+        //1. Vòng lặp For
+        for(int i = 0; i < 3; i++){
+            System.out.println(i);
+        }
+    }
+}
