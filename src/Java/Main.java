888cddf811a5bfc5ccc2bb8da70cb87a50bc78c5
Nguyễn Trọng Thức <24110349@student.hcmute.edu.vn>
2026-08-18 19:11:10 +0700

    #13 Câu lệnh điều kiện Switch Case

--- src/Java/Main.java
+++ src/Java/Main.java
@@ -46,15 +46,43 @@ public class Main {
 //        } else {
 //            System.out.println("Fail");
 //        }
-        Scanner input = new Scanner(System.in);
-        System.out.print("Nhập vào danh thu của bạn: ");
-        int tax = input.nextInt();
-        if(tax < 10){
-            System.out.println("Không đón thuees");
-        } else if ( tax >= 10 && tax <= 15){
-            System.out.println("Đóng thuế 10%");
-        } else{
-            System.out.println("Đóng thuế 20%");
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Nhập vào danh thu của bạn: ");
+//        int tax = input.nextInt();
+//        if(tax < 10){
+//            System.out.println("Không đón thuees");
+//        } else if ( tax >= 10 && tax <= 15){
+//            System.out.println("Đóng thuế 10%");
+//        } else{
+//            System.out.println("Đóng thuế 20%");
+//        }
+
+//        Bài 13: Câu lệnh điều kiện Switch Case
+        int day = 4;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("I don't know");
         }
     }
 }
