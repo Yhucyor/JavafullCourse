d63ef9f0fe71fa84de05e1dea3c306caaf540356
Nguyễn Trọng Thức <24110349@student.hcmute.edu.vn>
2026-08-18 20:11:19 +0700

    #Chuong4: Kiểu dữ liệu mảng

---
+++ src/Java/Chuong4.java
@@ -0,0 +1,13 @@
+package Java;
+
+import java.util.Arrays;
+
+public class Chuong4 {
+    // === CHƯƠNG 4: KIỂM DỮ LIỆU ARRAY === //
+    public static void main(String[] args) {
+        String[] cars;
+        cars = new String[] {"Volvo", "BMW", "Ford", "Mazda"};
+        System.out.println(Arrays.toString(cars));
+        System.out.println(cars.length);
+    }
+}
