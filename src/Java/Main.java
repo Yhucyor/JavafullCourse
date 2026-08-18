c20365c3a5e9d4b3ea5645c9f34fddc3d29d6a35
Nguyễn Trọng Thức <24110349@student.hcmute.edu.vn>
2026-08-18 19:01:13 +0700

    #12 Câu lệnh điều kiện

--- src/Java/Main.java
+++ src/Java/Main.java
@@ -37,5 +37,24 @@ public class Main {
 //                and: &&, or: ||, not: !
 
         // CHƯƠNG 2: CÁC CÂU LỆNH ĐIỀU KIỆN
+//        Bài 12
+//        int score = 9;
+//        if(score >= 9){
+//            System.out.println("Good");
+//        } else if(score >= 8){
+//            System.out.println("Pass");
+//        } else {
+//            System.out.println("Fail");
+//        }
+        Scanner input = new Scanner(System.in);
+        System.out.print("Nhập vào danh thu của bạn: ");
+        int tax = input.nextInt();
+        if(tax < 10){
+            System.out.println("Không đón thuees");
+        } else if ( tax >= 10 && tax <= 15){
+            System.out.println("Đóng thuế 10%");
+        } else{
+            System.out.println("Đóng thuế 20%");
+        }
     }
 }
