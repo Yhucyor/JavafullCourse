30e802825a0f43ab8dc7cc47b35b108b5e6120af
Nguyễn Trọng Thức <24110349@student.hcmute.edu.vn>
2026-08-18 21:17:12 +0700

    #OOP: Method và Attribute

--- src/Java/OOP/Student.java
+++ src/Java/OOP/Student.java
@@ -5,11 +5,17 @@ public class Student {
     // Class là đối tượng tổng quát của real-world được mô phỏng
     // Object là đối tượng cụ thể - thực thể chi tiết của thực thể đấy
 
+    // Thuộc tính của đối tượng Atrribute
     String name;
     int age;
+    String address;
 
+    // Phương thức của đối tượng Method
     public void printInfo(){
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
+    }
+    void setAddress(String address){
+        this.address = address;
     }
 }
