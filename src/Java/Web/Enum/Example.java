2c5baba9a37481c762ffe729216d8a41dbb99ae4
Nguyễn Trọng Thức <24110349@student.hcmute.edu.vn>
2026-08-19 18:25:29 +0700

    #Enum: Enum cơ bản

---
+++ src/Java/Web/Enum/Example.java
@@ -0,0 +1,7 @@
+package Java.Web.Enum;
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println(NgayTrongTuan.THU_HAI.ordinal());
+    }
+}
---
+++ src/Java/Web/Enum/NgayTrongTuan.java
@@ -0,0 +1,9 @@
+package Java.Web.Enum;
+
+public enum NgayTrongTuan {
+//    Enum cơ bản hay sử dụng
+    THU_HAI,
+    THU_BA,
+    THU_TU,
+    THU_NAM
+}
